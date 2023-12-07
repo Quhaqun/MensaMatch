56caf5e24efe4bdaffdfe74c575c5ffd4256f22c
Mohamed Abu-Shawish <abu-shawish@campus.tu-berlin.de>
2023-12-07 23:55:13 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/mensa_match/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mensa_match
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'values/app_theme.dart';
+import 'pages/login_page.dart';
+import 'pages/register_page.dart';
+import 'values/app_constants.dart';
+import 'values/app_routes.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  ).then(
+        (_) => runApp(
+      const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Theme.of(context);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mensa Match',
+      theme: AppTheme.themeData,
+      initialRoute: AppRoutes.loginScreen,
+      navigatorKey: AppConstants.navigationKey,
+      routes: {
+        AppRoutes.loginScreen: (context) => const LoginPage(),
+        AppRoutes.registerScreen: (context) => const RegisterPage(),
+      },
+    );
+  }
+}
