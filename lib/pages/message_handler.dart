770fd3bf16be0b8561ebc143466de0df5cb44561
Viola-Joanna Stamer <52070257+joannasta@users.noreply.github.com>
2024-01-04 14:06:45 +0100

    Update message_handler.dart

--- lib/pages/message_handler.dart
+++ lib/pages/message_handler.dart
@@ -1,28 +1,33 @@
-// message_handler.dart
 import 'dart:math';
-
+import 'package:uuid/uuid.dart';
 import 'chat_message.dart';
 
 List<ChatMessage> messages = [];
+Function() onMessagesChanged = () {}; // Callback for notifying listeners
 
 ChatMessage createMessage(String text, bool isSentByUser, {String sender = 'Default Sender'}) {
   final newMessage = ChatMessage(
-    Random().nextInt(1000000).toString(), // Generate a unique identifier
-    text,
-    isSentByUser,
+    documentId: Uuid().v4(),
+    text: 'Your actual text here',
+    isSentByUser: true, // or false based on your logic
+    timestamp: DateTime.now().millisecondsSinceEpoch,
   );
   messages.add(newMessage);
+  onMessagesChanged(); // Notify listeners
   return newMessage;
 }
 
-
 void deleteMessage(ChatMessage message) {
   messages.remove(message);
+  onMessagesChanged(); // Notify listeners
 }
 
 void updateMessage(String documentId, String newText) {
-  final message = messages.firstWhere((msg) => msg.documentId == documentId);
-  message.text = newText;
+  final index = messages.indexWhere((message) => message.documentId == documentId);
+  if (index != -1) {
+    messages[index] = messages[index].copyWithNewText(newText);
+    onMessagesChanged(); // Notify listeners
+  }
 }
 
 void showStatus(ChatMessage message) {
