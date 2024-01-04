5934554a8541d7a1550532ce180b08783d1d6eda
Viola-Joanna Stamer <52070257+joannasta@users.noreply.github.com>
2024-01-04 14:06:31 +0100

    Update main.dart

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:appwrite/appwrite.dart';
 import 'chat_screen.dart';
-import 'toolbar.dart';
-import 'custom_app_bar.dart';
 
 void main() {
-  runApp(MyApp());
+  final client = Client()
+      .setEndpoint('https://god-did.de/v1') // Set your Appwrite endpoint
+      .setProject('657c5f8ee668aff8af1f') // Set your Appwrite project ID
+      .setSelfSigned();
+
+  runApp(MyApp(client: client));
 }
 
 class MyApp extends StatelessWidget {
+  final Client client;
+
+  const MyApp({Key? key, required this.client}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: CustomAppBar(),
-        body: Padding(
-          padding: EdgeInsets.only(top: 20,bottom:20),
-          child: ChatScreen(),
-        ),
+      title: 'Your App Title',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: ChatScreen(),  // Remove 'client:'
     );
   }
 }
+
+
+
+Future<void> initializeAppwrite(Client client) async {
+    // Add any Appwrite initialization logic here
+    // For example: Authenticate the user, initialize databases, etc.
+    // You can use 'client' to interact with Appwrite services.
+  }
