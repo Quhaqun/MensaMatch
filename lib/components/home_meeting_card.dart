1a9bcba7a26d660e195c36a80d63e5c7581f7aae
Mohamed Abu-Shawish <abu-shawish@campus.tu-berlin.de>
2024-01-25 05:11:18 +0100

    working match

--- lib/components/home_meeting_card.dart
+++ lib/components/home_meeting_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:mensa_match/constants/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,18 +8,17 @@ class HomeMeetingCard extends StatelessWidget {
   final String time;
   final String location;
 
-  const HomeMeetingCard(
-      {super.key,
-      required this.imageUrl,
-      required this.name,
-      required this.time,
-      required this.location});
+  const HomeMeetingCard({
+    Key? key,
+    required this.imageUrl,
+    required this.name,
+    required this.time,
+    required this.location,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 240.0,
-      height: 120.0,
       margin: EdgeInsets.only(right: 16.0),
       padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
@@ -41,38 +39,36 @@ class HomeMeetingCard extends StatelessWidget {
             ),
           ),
           SizedBox(width: 16.0),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  name,
-                  style: GoogleFonts.roboto(
-                    color: AppColors.textColorDark,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20,
-                    height: 1.0,
-                  ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                name,
+                style: GoogleFonts.roboto(
+                  color: AppColors.textColorDark,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                  height: 1.0,
                 ),
-                Text(
-                  time,
-                  style: GoogleFonts.roboto(
-                    color: AppColors.textColorDark,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                  ),
+              ),
+              Text(
+                time,
+                style: GoogleFonts.roboto(
+                  color: AppColors.textColorDark,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
                 ),
-                Text(
-                  location,
-                  style: GoogleFonts.roboto(
-                    color: AppColors.textColorDark,
-                    fontWeight: FontWeight.w300,
-                    fontSize: 16,
-                  ),
+              ),
+              Text(
+                location,
+                style: GoogleFonts.roboto(
+                  color: AppColors.textColorDark,
+                  fontWeight: FontWeight.w300,
+                  fontSize: 16,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
